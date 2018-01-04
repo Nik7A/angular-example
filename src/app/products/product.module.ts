4d69e37de44f571881bf84a8e54a688c9a695a02
Nikolay Semernya <nsemernia@gmail.com>
2018-01-04 12:44:16 +0530

    Separated product module

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProductGuardService } from './product-guard.service';
+import { ProductService } from './product.service';
+
+import { ProductListComponent } from './product-list.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { ProductDetailComponent } from './product-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', 
+        canActivate: [ ProductGuardService ],
+        component: ProductDetailComponent },
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+  ],
+  providers: [
+    ProductService,
+    ProductGuardService
+  ]
+})
+export class ProductModule { }
